9469597e1b6506f6011ebc89da0bef38a0655412
Be4rCl4w <121987346+Be4rCl4w@users.noreply.github.com>
2026-08-28 19:19:07 +0100

    Rename service plan from 'example' to 'Myexample'

--- App.tf
+++ App.tf
@@ -176,7 +176,7 @@ resource "azurerm_subnet_network_security_group_association" "nsg_association" {
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "example"
+  name                = "Myexample"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
